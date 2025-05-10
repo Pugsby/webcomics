c91208a5713a237f45541b10c5aade5700ed5957
Pugsby <pugsbypuggerson@duck.com>
2025-05-10 16:05:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ function openComic(source, name, description) {
     document.getElementById("comicName").innerText = name;
     document.getElementById("description").innerHTML = description;
 }
+
+function attachHoverSound() {
+    document.querySelectorAll('button').forEach(button => {
+        if (!button.hasAttribute('data-hover-sound')) {
+            button.setAttribute('data-hover-sound', 'true');
+            button.addEventListener('mouseover', () => {
+                const audio = new Audio('hover.mp3');
+                audio.play();
+            });
+            button.addEventListener('click', () => {
+                const audio = new Audio('click.mp3');
+                audio.play();
+            });
+        }
+    });
+}
+
+attachHoverSound();
+
+const observer = new MutationObserver(() => {
+    attachHoverSound();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
